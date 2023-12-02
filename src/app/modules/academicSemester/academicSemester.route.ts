1ce04b1c17c2d1420167223e40d4a90525c83d38
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-02 12:32:54 +0600

    semester controller created

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,7 +1,8 @@
 import express from 'express';
+import { SemesterControllers } from './academicsemester.controller';
 
 const router = express.Router();
 
-router.post('/create-semester');
+router.post('/create-semester', SemesterControllers.createSemester);
 
 export const SemesterRoutes = router;
