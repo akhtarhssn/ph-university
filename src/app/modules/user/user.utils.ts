16cd8122c82893eb8547942a19abffdc1f07aeba
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-05 10:29:15 +0600

    unique id bug fixed

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -5,7 +5,7 @@ const findLastEnrolledStudent = async () => {
   const lastStudent = await User.findOne({ role: 'Student' }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 // auto generated code --> year semesterCode 4 digit number
@@ -14,7 +14,23 @@ export const generateStudentId = async (payload: IAcademicSemester) => {
     throw new Error('Invalid payload'); // or handle this case as appropriate
   }
 
-  const currentId = (await findLastEnrolledStudent()) || (0).toString();
+  let currentId = (0).toString(); //by default
+
+  const lastStudentId = await findLastEnrolledStudent();
+
+  const lastStudentSemesterYear = lastStudentId?.substring(0, 4);
+  const lastStudentSemesterCode = lastStudentId?.substring(4, 6);
+  const currentSemesterYear = payload.year;
+  const currentSemesterCode = payload.code;
+
+  if (
+    lastStudentId &&
+    lastStudentSemesterYear === currentSemesterYear &&
+    lastStudentSemesterCode === currentSemesterCode
+  ) {
+    currentId = lastStudentId.substring(6);
+  }
+
   let incrementId = (Number(currentId) + 1).toString().padStart(4, '0');
 
   incrementId = `${payload.year}${payload.code}${incrementId}`;
