63dcb2ba24a124f26bf4ac0eadb6daf525bd2e24
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-13 04:26:13 +0600

    Course interface and model created

---
+++ src/app/modules/Course/course.model.ts
@@ -0,0 +1,37 @@
+import { Schema, model } from 'mongoose';
+import { TCourse, TPreRequisiteCourses } from './course.interface';
+
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
+  course: {
+    type: Schema.Types.ObjectId,
+  },
+  isDeleted: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const courseSchema = new Schema<TCourse>({
+  title: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+  },
+  prefix: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  code: {
+    type: Number,
+    trim: true,
+  },
+  credits: {
+    type: Number,
+    trim: true,
+  },
+  preRequisiteCourses: [preRequisiteCoursesSchema],
+});
+
+export const CourseModel = model<TCourse>('Course', courseSchema);
