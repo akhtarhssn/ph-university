a5de2f0cff8aacbcc3222a797b77945876941d4a
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-13 04:48:52 +0600

    course route completed

---
+++ src/app/modules/Course/course.route.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import validateRequest from '../../middleware/validateRequest';
+import { CourseZodValidation } from './course.validation';
+import { CourseControllers } from './course.controller';
+
+const router = express.Router();
+
+router.post(
+  '/create-course',
+  validateRequest(CourseZodValidation.CreateCourseValidation),
+  CourseControllers.createCourse,
+);
+
+router.get('/', CourseControllers.getAllCourses);
+
+router.get('/:courseId', CourseControllers.getSingleCourse);
+
+router.patch('/:courseId', CourseControllers.deleteCourse);
+
+export const CourseRouter = router;
