5253bfa5b63dde4f0a224db9b3054bd3980ba0ac
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-13 14:06:17 +0600

    removed the _id field from prequisite course

--- src/app/modules/Course/course.model.ts
+++ src/app/modules/Course/course.model.ts
@@ -1,16 +1,22 @@
 import { Schema, model } from 'mongoose';
 import { TCourse, TPreRequisiteCourses } from './course.interface';
 
-const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>({
-  course: {
-    type: Schema.Types.ObjectId,
-    ref: 'Course',
+const preRequisiteCoursesSchema = new Schema<TPreRequisiteCourses>(
+  {
+    course: {
+      type: Schema.Types.ObjectId,
+      ref: 'Course',
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
   },
-  isDeleted: {
-    type: Boolean,
-    default: false,
+  {
+    _id: false,
+    timestamps: true,
   },
-});
+);
 
 const courseSchema = new Schema<TCourse>({
   title: {
