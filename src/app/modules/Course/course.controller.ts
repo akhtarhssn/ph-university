b51ae79fbd52afea1761e5e41353dbf6b442991b
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-23 23:54:43 +0600

    a todo list created

--- src/app/modules/Course/course.controller.ts
+++ src/app/modules/Course/course.controller.ts
@@ -1,7 +1,7 @@
 import httpStatus from 'http-status';
 import catchAsync from '../../utils/catchAsync';
 import sendResponse from '../../utils/sendResponse';
-import { CourseServices } from './course.servicec';
+import { CourseServices } from './course.service';
 
 const createCourse = catchAsync(async (req, res) => {
   const result = await CourseServices.createCourse(req.body);
