82d44b15f1d62e727d487eb07ca4ebf85a6c802c
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-25 14:31:29 +0600

    Semester Regsitration interface and model created

--- src/app/modules/semesterRegistration/semesterRegistration.interface.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.interface.ts
@@ -1,1 +1,10 @@
-export interface ISemesterRegistration {}
+import { Types } from 'mongoose';
+
+export interface ISemesterRegistration {
+  academicSemester: Types.ObjectId;
+  status: 'UPCOMING' | 'ONGOING' | 'ENDED';
+  startDate: Date;
+  endDate: Date;
+  minCredit: number;
+  maxCredit: number;
+}
--- src/app/modules/semesterRegistration/semesterRegistration.model.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -1,9 +1,36 @@
-import mongoose from 'mongoose';
+import mongoose, { Schema } from 'mongoose';
 import { ISemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistrationStatus } from './semesterRegistration.constants';
 
-const semesterRegistrationSchema = new mongoose.Schema<ISemesterRegistration>(
-  {},
-);
+const semesterRegistrationSchema = new mongoose.Schema<ISemesterRegistration>({
+  academicSemester: {
+    type: Schema.Types.ObjectId,
+    ref: 'Semester',
+    unique: true,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: SemesterRegistrationStatus,
+    default: 'UPCOMING',
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    remove: true,
+  },
+  minCredit: {
+    type: Number,
+    remove: true,
+  },
+  maxCredit: {
+    type: Number,
+    required: true,
+  },
+});
 
 export const SemesterRegistrationModel = mongoose.model<ISemesterRegistration>(
   'SemesterRegistration',
