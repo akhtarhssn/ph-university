a7df1ef6bcf5c69625bc132276fb03064303885a
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-25 14:36:16 +0600

    Semester Regsitration validation created

--- src/app/modules/semesterRegistration/semesterRegistration.validation.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.validation.ts
@@ -1,10 +1,27 @@
 import { z } from 'zod';
+import { SemesterRegistrationStatus } from './semesterRegistration.constants';
 
 const CreateSemesterRegistrationZodSchema = z.object({
-  body: z.object({}),
+  body: z.object({
+    academicSemester: z.string(),
+    status: z.enum([...(SemesterRegistrationStatus as [string, ...string[]])]),
+    startDate: z.string().datetime(),
+    endDate: z.string().datetime(),
+    minCredit: z.number(),
+    maxCredit: z.number(),
+  }),
 });
 const UpdateSemesterRegistrationZodSchema = z.object({
-  body: z.object({}),
+  body: z.object({
+    academicSemester: z.string().optional(),
+    status: z
+      .enum([...(SemesterRegistrationStatus as [string, ...string[]])])
+      .optional(),
+    startDate: z.string().datetime().optional(),
+    endDate: z.string().datetime().optional(),
+    minCredit: z.number().optional(),
+    maxCredit: z.number().optional(),
+  }),
 });
 
 export const SemesterRegistrationValidation = {
