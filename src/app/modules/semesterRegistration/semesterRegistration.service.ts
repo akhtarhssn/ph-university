d8df15278aeeb3f66f2f515a0695ef830c71db99
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-25 15:59:00 +0600

    Semester registration create service

--- src/app/modules/semesterRegistration/semesterRegistration.service.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.service.ts
@@ -1,4 +1,34 @@
-const createSemesterRegistration = async () => {};
+import httpStatus from 'http-status';
+import { AppError } from '../../errors/AppError';
+import { SemesterModel } from '../academicSemester/academicSemester.model';
+import { ISemesterRegistration } from './semesterRegistration.interface';
+import { SemesterRegistrationModel } from './semesterRegistration.model';
+
+const createSemesterRegistration = async (payload: ISemesterRegistration) => {
+  const academicSemester = payload?.academicSemester;
+  const isSemesterRegistrationExists = await SemesterRegistrationModel.findOne({
+    academicSemester,
+  });
+
+  // throw new Error if we don't have a Semester matching
+  if (academicSemester) {
+    const isAcademicSemester = await SemesterModel.findById(academicSemester);
+    if (!isAcademicSemester) {
+      throw new AppError(
+        httpStatus.NOT_FOUND,
+        'The semester registration not found',
+      );
+    }
+
+    // throw error if the semester registration exists
+    if (isSemesterRegistrationExists) {
+      throw new AppError(
+        httpStatus.CONFLICT,
+        `${academicSemester} already exists`,
+      );
+    }
+  }
+};
 const getAllSemesterRegistration = async () => {};
 const getSingleSemesterRegistration = async () => {};
 const updateSemesterRegistration = async () => {};
