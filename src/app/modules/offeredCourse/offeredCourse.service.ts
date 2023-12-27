cbb86d4c0f33eb8b328113e31c66147948b86230
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-27 23:21:50 +0600

    comparing the schedule

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -94,7 +94,12 @@ const createOfferedCourse = async (payload: IOfferedCourse) => {
     endTime,
   };
 
-  assignedSchedules.forEach((schedule) => {});
+  assignedSchedules.forEach((schedule) => {
+    const existingStartTime = new Date(`1970-01-01T${schedule.startTime}`);
+    const existingEndTime = new Date(`1970-01-01T${schedule.endTime}`);
+    const newStartTime = new Date(`1970-01-01T${newSchedule.startTime}`);
+    const newEndTime = new Date(`1970-01-01T${newSchedule.endTime}`);
+  });
 
   // const result = await OfferedCourseModel.create({
   //   ...payload,
