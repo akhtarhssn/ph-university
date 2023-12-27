289eeac6bcba727bec4df1c22d8059ee9d719a5e
Mohammed Akhtar <watchonlytutorial@gmail.com>
2023-12-28 03:09:37 +0600

    refactored: auth login service file refactored

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -6,14 +6,21 @@ import { ILoginUser } from './auth.interface';
 const loginUser = async (payload: ILoginUser) => {
   // check if the user exists
   const isUserExist = await User.findOne({ id: payload?.id });
-  console.log(isUserExist);
+  const isDeleted = isUserExist?.isDeleted;
+  const status = isUserExist?.status;
 
-  if (!isUserExist || isUserExist.isDeleted === true) {
+  if (!isUserExist || isDeleted === true || status === 'Blocked') {
     throw new AppError(
       httpStatus.NOT_FOUND,
-      `${!isUserExist ? 'User not found' : 'This user is deleted'}`,
+      `${
+        (!isUserExist && 'User not found !') ||
+        (isDeleted && 'This user is deleted !!!') ||
+        'The user is blocked !!!'
+      }`,
     );
   }
+
+  // Access Granted: Send AccessToken, RefreshToken
 };
 
 export const AuthServices = {
