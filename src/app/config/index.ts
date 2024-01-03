b676a0dc7fab0cf5a67f854732a5e568c9e1924a
Mohammed Akhtar <watchonlytutorial@gmail.com>
2024-01-04 04:42:17 +0600

    updated

--- src/app/config/index.ts
+++ src/app/config/index.ts
@@ -13,4 +13,6 @@ export default {
   jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
   jwt_access_expires: process.env.JWT_ACCESS_EXPIRES_IN,
   jwt_refresh_expires: process.env.JWT_REFRESH_EXPIRES_IN,
+  smtp_pass: process.env.SMTP_PASS,
+  mail_sender: process.env.MAIL_SENDER,
 };
--- src/app/utils/sendMail.ts
+++ src/app/utils/sendMail.ts
@@ -2,15 +2,14 @@ import nodemailer from 'nodemailer';
 import config from '../config';
 
 export const sendMail = async (to: string, html: string) => {
-  console.log(to, html);
   const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
     port: 587,
     secure: config.node_env === 'production',
     auth: {
       // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-      user: 'hasnahena1420@gmail.com',
-      pass: 'zyec rwsx jytl tmpd',
+      user: config.mail_sender,
+      pass: config.smtp_pass,
     },
   });
 
@@ -18,8 +17,8 @@ export const sendMail = async (to: string, html: string) => {
   await transporter.sendMail({
     from: '"PH University " <support@ph-university.com>', // sender address
     to, // list of receivers
-    subject: 'Password Reset Request', // Subject line
-    text: 'Below is your password reset link. This link is valid for 10 minutes', // plain text body
+    subject: 'Password Reset Request, This link is valid for 10 minutes', // Subject line
+    text: '', // plain text body
     html, // html body
   });
 };
